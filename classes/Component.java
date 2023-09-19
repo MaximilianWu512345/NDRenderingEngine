e53ceb25ae8219bb262830d537a1b796389b4f93
Chi <1573027@fcps.edu>
2023-09-19 10:53:17 -0400

    Added Comments

--- classes/Component.java
+++ classes/Component.java
@@ -2,16 +2,25 @@ import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JComponent;
 
+/** Container class to hold JComponents, acts like a JPanel that doesn't have to be added. */
 public class Component {
    
+/** Determines location of component. */
    protected int x, y;
    
+/** Determines size of component. */
    protected int width, height;
    
+/** Determines color of component. */
    protected Color color;
 
 /**
 * Creates a new Component at location (x, y) with size (w, h), with a color of c.
+* @param x the x-coord of the location of the component.
+* @param y the y-coord of the location of the component.
+* @param w the width of the component.
+* @param h the height of the component.
+* @param c the color of the component.
 */
    public Component(int x, int y, int w, int h, Color c) {
       this(x, y, w, h);
@@ -20,6 +29,10 @@ public class Component {
 
 /**
 * Creates a new Component at location (x, y) with size (w, h).
+* @param x the x-coord of the location of the component.
+* @param y the y-coord of the location of the component.
+* @param w the width of the component.
+* @param h the height of the component.
 */
    public Component(int x, int y, int w, int h) {
       setLocation(x, y);
@@ -28,6 +41,8 @@ public class Component {
 
 /**
 * Changes size to be (w, h).
+* @param w the width to set the component to.
+* @param h the height to set the component to.
 */
    public void setSize(int w, int h) {
       width = w;
@@ -36,6 +51,8 @@ public class Component {
 
 /**
 * Changes location to be (x, y).
+* @param x the x-coord to set the location to.
+* @param y the y-coord to set the location to.
 */
    public void setLocation(int x, int y) {
       this.x = x;
@@ -44,6 +61,7 @@ public class Component {
    
 /**
 * Sets color to c.
+* @param c the color to change the component to.
 */
    public void setColor(Color c) {
       color = c;
@@ -51,7 +69,7 @@ public class Component {
 
 /**
 * Returns the x value of this object's location.
-* @return   an int of the x value of this object's location.
+* @return an int of the x value of this object's location.
 */
    public int getX() {
       return x;
@@ -59,7 +77,7 @@ public class Component {
    
 /**
 * Returns the y value of this object's location.
-* @return   an int of the y value of this object's location.
+* @return an int of the y value of this object's location.
 */
    public int getY() {
       return y;
@@ -67,7 +85,7 @@ public class Component {
    
 /**
 * Returns the width of this object's size.
-* @return   an int of the width of this object's size.
+* @return an int of the width of this object's size.
 */
    public int getWidth() {
       return width;
@@ -75,7 +93,7 @@ public class Component {
    
 /**
 * Returns the height of this object's size.
-* @return   an int of the height of this object's size.
+* @return an int of the height of this object's size.
 */
    public int getHeight() {
       return height;
@@ -83,7 +101,7 @@ public class Component {
    
 /**
 * Returns the Color of this object.
-* @return   the Color of this object.
+* @return the Color of this object.
 */
    public Color getColor() {
       return color;
@@ -91,7 +109,7 @@ public class Component {
 
 /**
 * Returns an array of JComponents, representing components inside of extended classes of this container Component. Otherwise, defaults to null.
-* @return   an array of JComponents, representing components inside of extended classes of this container Component. Otherwise, defaults to null.
+* @return an array of JComponents, representing components inside of extended classes of this container Component. Otherwise, defaults to null.
 */
    public JComponent[] getComponents() {
       return null;
@@ -99,6 +117,7 @@ public class Component {
    
 /**
 * Paints components inside of extended classes of this container Component. Otherwise, does nothing.
+* @param g uses graphics to paint components inside this component container class.
 */
    protected void paintComponent(Graphics g) { }
 }
--- classes/Engine.java
+++ classes/Engine.java
@@ -5,12 +5,19 @@ import java.util.Hashtable;
 import java.awt.Color;
 import java.awt.Graphics;
 
+/** JFrame engine that creates a graphical application. */
 public class Engine extends JFrame {
 
+/** Title of JFrame when program is run. */
    protected static final String TITLE = "Max Wu's Concoction Machine";
    
+/** JPanel for engine. */
    protected EnginePanel panel;
 
+/** Creates a new Engine JFrame with size (width, height), and calls initialize(width, height).
+* @param width the width of the engine JFrame.
+* @param height the height of the engine JFrame.
+*/
    public Engine(int width, int height) {
       super(TITLE);
       initialize(width, height);
@@ -19,10 +26,17 @@ public class Engine extends JFrame {
       setVisible(true);
    }
    
+/** Creates JPanel for engine with (width, height).
+* @param width the width of the engine JPanel.
+* @param height the height of the engine JPanel.
+*/
    private void initialize(int width, int height) {
       add(panel = new EnginePanel(width, height));
    }
 
+/** Placeholder method to run TestDriver.
+* @param args default args for main method.
+*/
    public static void main(String[] args) {
       TestDriver.main(args);
    }
